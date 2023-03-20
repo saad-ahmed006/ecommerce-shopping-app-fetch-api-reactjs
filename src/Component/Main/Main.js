f47a4d0d0d1fa3a3079220df26418e3187f39ab0
saad-ahmed006 <muhammadsaad368444@gmail.com>
2023-03-20 20:40:53 +0500

    main-component

--- src/Component/Main/Main.js
+++ src/Component/Main/Main.js
@@ -1,7 +1,25 @@
+import { Grid, Typography } from '@mui/material'
+import Box from '@mui/material/Box'
 import React from 'react'
+import Image from '../../Assets/pexels-photo-432059.webp'
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <>
+
+      <Grid container style={{ backgroundColor: 'grey', height: '456px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+
+        <Grid item sm={12} md={6} style={{ textAlign: 'center' }}>
+          <Box><Typography variant='h3' color={'#4d0101'} fontWeight={'bold'}  >NEW SEASON ARRIVALS</Typography></Box>
+          <Box><Typography variant='h5'>CheckOut All The Trends</Typography></Box>
+        </Grid>
+        <Grid item sm={12} md={6} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <img style={{ borderTopRightRadius: '80px', width: '350px' }} src={Image} alt={'MainImage'}></img>
+        </Grid>
+
+      </Grid>
+
+    </>
+
   )
 }
